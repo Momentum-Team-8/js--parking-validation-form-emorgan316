4b25bc35e39c5e93b99006c1dc77506199047bbd
emorgan316 <elliemorgan96@gmail.com>
2021-05-23 17:19:16 -0400

    step 4

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Add validation!');
+function calculatePayment(numberOfDays, chargePerDay){
+    return numberOfDays * chargePerDay;
+}
+function onSubmit(event){
+    event.preventDefault();
+    var days=document.getElementById("days").value;
+    var price=5;
+    document.getElementById("total").innerHTML = calculatePayment(days,price);
+}
