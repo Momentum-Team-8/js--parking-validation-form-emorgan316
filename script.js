4debaf7bba5a3b739d618bb5c0a62a08cb54600f
emorgan316 <elliemorgan96@gmail.com>
2021-05-24 20:20:34 -0400

    Roan helped debug event listener

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ function onSubmit(event){
     var price=5;
     document.getElementById("total").innerHTML = calculatePayment(days,price);
 }
+
+
+let ccnumber = document.querySelector('input[id="credit-card"]')
+document.addEventListener (‘click’, event => {
+    console.log(ccnumber)
+})
+
+
+function validateCardNumber(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
+}
