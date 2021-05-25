3dbbe0fdd823e40472bf0702750787b2f57ff574
emorgan316 <elliemorgan96@gmail.com>
2021-05-24 20:51:20 -0400

    Roan helped debug console log

--- script.js
+++ script.js
@@ -11,18 +11,27 @@ function onSubmit(event){
 
 
 let ccnumber = document.querySelector('input[id="credit-card"]')
-document.addEventListener ("click", event => {
-    console.log(ccnumber)
-})
+// document.addEventListener ("click", event => {
+//     console.log(ccnumber.value)
+// })
 
 
 function validateCardNumber(number) {
     var regex = new RegExp("^[0-9]{16}$");
-    if (!regex.test(number))
-        return false;
+    if (!regex.test(number)){
+        console.log(false);
+    } else {
+        console.log(true); 
+    }
+         
+    // return luhnCheck(number);
 
-    return luhnCheck(number);
 }
+
+document.addEventListener ("click", event => {
+    validateCardNumber(ccnumber.value)
+    })
+
 
 function luhnCheck(val) {
     var sum = 0;
